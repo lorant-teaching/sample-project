926f517d8a0a50756e7b8da6345c014bfb1284ad
chvatalp <56312836+chvatalp@users.noreply.github.com>
2020-03-12 19:41:18 +0100

    Update CountryExtractorTest.java

--- test/com/engeto/CountryExtractorTest.java
+++ test/com/engeto/CountryExtractorTest.java
@@ -13,20 +13,19 @@ public class CountryExtractorTest {
     @Test
     public void extractCountriesTest() {
         JsonDownloader jsonDownloader = new JsonDownloader();
-        GetRequest request = jsonDownloader.getRequestFrom("http://jsonvat.com/");
+        GetRequest request = jsonDownloader.getRequestFrom("https://my-json-server.typicode.com/chvatalp/sample-api/db");
         JSONObject jsonObject = jsonDownloader.getAsJson(request);
         ArrayList<Country> countries = new CountryExtractor().extractCountries(jsonObject);
 
-
         for (Country country : countries) {
-            if (country.getName().equals("Spain")) {
-                Assert.assertEquals("Spain", country.getName());
+            if (country.getName().equals("SampleCountry")) {
+                Assert.assertEquals("SampleCountry", country.getName());
             }
-            if (country.getName().equals("Bulgaria")) {
-                Assert.assertEquals("BG", countries.get(1).getCode());
+            if (country.getName().equals("SampleCountry")) {
+                Assert.assertEquals("SC", country.getCode());
             }
-            if (country.getName().equals("Hungary")) {
-                Assert.assertEquals(27.0, countries.get(2).getStandardRate(), 0.01);
+            if (country.getName().equals("SampleCountry2")) {
+                Assert.assertEquals(21.0, country.getStandardRate(), 0.01);
             }
         }
     }
